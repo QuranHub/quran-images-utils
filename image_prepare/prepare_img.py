c37f58d1b745ca59d9a4f57239792be1860e3aa0
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-05-06 21:40:46 +0300

    Finish prepare_img.py

--- image_prepare/prepare_img.py
+++ image_prepare/prepare_img.py
@@ -1,22 +1,41 @@
+#!/usr/bin/env python3 
+# Purpose: Prepare the Quran images
+# Author: Abdallah Abdelazim
+# Features:
+# - Crop the images to remove the borders.
+# - Resize & optimize the images to decrease the image size.
+# - The input images are expected to be in the images folder
+# - Save the output images in the output folder
+# - The input & output images are named as 001.jpg, 002.jpg, etc.
+# Pre-requisites:
+# - Python 3.6 or higher
+# - Pillow package
+
 import os
 from PIL import Image
 from pathlib import Path
+import subprocess
 
 # Define the input and output folder paths
 script_dir = os.path.dirname(__file__)
 input_folder = os.path.join(script_dir, "images")
 output_folder = os.path.join(script_dir, "output")
 
-# Define the offsets for even and odd pages: (left, right, top, bottom)
+# Define the crop offsets (pixels) for pages
+# (left, right, top, bottom)
 first_two_pages_offsets = (126, 126, 208, 208)
-even_offsets = (195, 129, 143, 143)
-odd_offsets = (128, 196, 143, 143)
+even_pages_offsets = (195, 129, 143, 143)
+odd_pages_offsets = (128, 196, 143, 143)
+
+# Define the resize ratio for pages
+resize_ratio = 0.5
 
 # Create the output folder if it doesn't exist
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
 # Loop through all the image files in the input folder
+# The image file name is expected to be like 001.jpg, 002.jpg, etc.
 for i in range(1, 605):
     # Construct the input and output file paths
     input_file = os.path.join(input_folder, f"{i:03}.jpg")
@@ -32,13 +51,13 @@ for i in range(1, 605):
     if i == 1 or i == 2:
         left_offset, right_offset, top_offset, bottom_offset = first_two_pages_offsets
     elif i % 2 == 0:
-        left_offset, right_offset, top_offset, bottom_offset = even_offsets
+        left_offset, right_offset, top_offset, bottom_offset = even_pages_offsets
     else:
-        left_offset, right_offset, top_offset, bottom_offset = odd_offsets
+        left_offset, right_offset, top_offset, bottom_offset = odd_pages_offsets
 
     # Calculate the new image dimensions
-    new_width = width - (left_offset + right_offset)
-    new_height = height - (top_offset + bottom_offset)
+    new_width = int((width - (left_offset + right_offset)) * resize_ratio)
+    new_height = int((height - (top_offset + bottom_offset)) * resize_ratio)
 
     # Crop the image
     cropped_image = image.crop((left_offset, top_offset, width - right_offset, height - bottom_offset))
@@ -47,6 +66,9 @@ for i in range(1, 605):
     cropped_image = cropped_image.resize((new_width, new_height))
 
     # Save the cropped image
-    cropped_image.save(output_file)
+    cropped_image.save(output_file, optimize=True, quality=85)
 
     print(f"Prepared {i} of 604")
+
+print("Done preparing images")
+
